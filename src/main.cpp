79169db5ee0ae65b6dcf8688323720098da29465
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2026-01-29 17:47:30 -0500

    simple testcases

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,44 @@
+// src/main.cpp
 #include <iostream>
-#include <thread>
-#include <vector>
-#include <chrono>
-#include <atomic>
-#include <iomanip>
-
-#include "mutex_queue.h"
+#include <cassert>
 #include "ring_buffer.h"
 #include "trace_span.h"
 
-// test constants
-const size_t QUEUE_CAPACITY = 1024;
-const size_t TOTAL_OPS = 10'000'000;
-const int THREAD_COUNT = 4;
-
-void benchMutexQueue()
-{
-    MutexQueue queue;
-    std::vector<std::thread> producers;
-    std::vector<std::thread> consumers;
-
-    size_t ops_per_thread = TOTAL_OPS / THREAD_COUNT;
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    // launch consumer threads
-    for (int i = 0; i < THREAD_COUNT; ++i)
-    {
-        consumers.emplace_back([&queue, ops_per_thread]()
-                               {
-            TraceSpan span;
-            for (size_t j = 0; j < ops_per_thread; ++j)
-            {
-                while (!queue.pop(span))
-                {
-                    std::this_thread::yield();
-                }
-            } });
-    }
-
-    // launch producer threads
-    for (int i = 0; i < THREAD_COUNT; ++i)
-    {
-        producers.emplace_back([&queue, ops_per_thread]()
-                               {
-            TraceSpan span{};
-            for (size_t j = 0; j < ops_per_thread; ++j)
-            {
-                queue.push(span);
-            } });
-    }
-
-    // join all
-    for (auto &t : producers)
-        t.join();
-    for (auto &t : consumers)
-        t.join();
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> duration = end - start;
-    double throughput = TOTAL_OPS / duration.count();
-
-    std::cout << "[MutexQueue] Time: " << std::setprecision(5) << duration.count() << " seconds | Throughput: " << (long)(throughput) << " ops/sec" << std::endl;
-}
-
-void benchRingBuffer()
-{
-    RingBuffer queue(QUEUE_CAPACITY);
-    std::vector<std::thread> producers;
-    std::vector<std::thread> consumers;
-
-    size_t ops_per_thread = TOTAL_OPS / THREAD_COUNT;
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    // launch consumer threads
-    for (int i = 0; i < THREAD_COUNT; ++i)
-    {
-        consumers.emplace_back([&queue, ops_per_thread]()
-                               {
-            TraceSpan span;
-            for (size_t j = 0; j < ops_per_thread; ++j)
-            {
-                // if empty, keep trying
-                while (!queue.pop(span))
-                {
-                    // optional, I'm being nice to my CPU here
-                    // std::this_thread::yield();
-                }
-            } });
-    }
-
-    // launch producer threads
-    for (int i = 0; i < THREAD_COUNT; ++i)
-    {
-        producers.emplace_back([&queue, ops_per_thread]()
-                               {
-            TraceSpan span{};
-            for (size_t j = 0; j < ops_per_thread; ++j)
-            {
-                // if full, keep trying
-                while (!queue.push(span))
-                {
-                    // std::this_thread::yield();
-                }
-            } });
-    }
-
-    // join all
-    for (auto &t : producers)
-        t.join();
-    for (auto &t : consumers)
-        t.join();
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> duration = end - start;
-    double throughput = TOTAL_OPS / duration.count();
-
-    std::cout << "[RingBuffer] Time: " << std::setprecision(5) << duration.count() << " seconds | Throughput: " << (long)(throughput) << " ops/sec" << std::endl;
-}
-
 int main()
 {
-    std::cout << "Benchmarking with " << TOTAL_OPS << " items and " << THREAD_COUNT << " threads." << std::endl;
+    RingBuffer rb(4);
 
-    benchMutexQueue();
-    benchRingBuffer();
+    // dummy span
+    TraceSpan in_span = {};
+    in_span.trace_id_high = 12345;
+    in_span.duration_ns = 500;
+
+    // testing push operation
+    if (rb.push(in_span))
+    {
+        std::cout << "[PASS] Push successful." << std::endl;
+    }
+    else
+    {
+        std::cerr << "[FAIL] Push failed on empty buffer!" << std::endl;
+        return 1;
+    }
+
+    // testing pop operation
+    TraceSpan out_span = {};
+    if (rb.pop(out_span))
+    {
+        std::cout << "[PASS] Pop successful." << std::endl;
+
+        // verify data integrity
+        assert(out_span.trace_id_high == 12345);
+        std::cout << "[PASS] Data integrity verified (ID: " << out_span.trace_id_high << ")." << std::endl;
+    }
+    else
+    {
+        std::cerr << "[FAIL] Pop failed on non-empty buffer!" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
